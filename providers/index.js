5ce3f9d236e44ce47be4d9c8ba52a4d20cd79647
Dmitrij Panasiuk <dmitrij.panasiuk@pinenet.io>
2023-12-07 13:31:40 +0200

    LP-1 TEST

--- providers/index.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import React, { createContext } from "react";
-
-const ThemeContext = createContext({});
-
-export default function ThemeProvider({ children }) {
-  return <ThemeContext.Provider value="dark">{children}</ThemeContext.Provider>;
-}
